4f6d02a6e2ed86959bc14143c4ca23fc90ca931a
prabhucdt <105945938+prabhucdt@users.noreply.github.com>
2024-01-08 21:49:14 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,3 +10,24 @@ resource "azurerm_resource_group" "test" {
 }
 }
 
+# Service Principal Variables
+
+variable "client_id" {
+    description =   "Client ID (APP ID) of the application"
+    type        =   string
+}
+
+variable "client_secret" {
+    description =   "Client Secret (Password) of the application"
+    type        =   string
+}
+
+variable "subscription_id" {
+    description =   "Subscription ID"
+    type        =   string
+}
+
+variable "tenant_id" {
+    description =   "Tenant ID"
+    type        =   string
+}
