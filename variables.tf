4dba1ac93e4df2a999a0369271f4720becee631b
AakashR3 <109940526+AakashR3@users.noreply.github.com>
2024-01-09 15:05:11 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,27 +1,8 @@
-variable "existing_resource_group_name" {
-  type    = string
-  default = "Aakash-Rg"
-}
-
-variable "existing_storage_account_name" {
-  type    = string
-  default = "tfteststg1"
-}
-
-variable "existing_container_name" {
-  type    = string
-  default = "tfstate"
-}
-
 variable "unique_id" {
   type    = string
   default = "default"
 }
 
-resource "random_string" "state_folder_suffix" {
-  length  = 6
-  special = false
-}
 # Service Principal Variables
 
 variable "client_id" {
