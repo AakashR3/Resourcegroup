2a933a0ff25a6c5e752ab4ac01282a6a1f0f0071
AakashR3 <109940526+AakashR3@users.noreply.github.com>
2024-01-09 16:50:27 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "Aakash-Rg"
     storage_account_name = "tfteststg1"
     container_name       = "tfstate"
-    key                  = "terraform.tfstate"
+    key                  = "\terraform.tfstate"
   }
 }
 
