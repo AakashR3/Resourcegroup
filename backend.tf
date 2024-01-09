913753de7da758ff1ee49b191a4605b8e1330f3d
AakashR3 <109940526+AakashR3@users.noreply.github.com>
2024-01-09 14:58:25 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,17 +1,10 @@
-#terraform {
-#  backend "azurerm" {
-#    resource_group_name  = "Aakash-Rg"
-#    storage_account_name = "tfteststg1"
-#    container_name       = "tfstate"
-#    key                  = "terraform.tfstate"
-#  }
-#}
 terraform {
   backend "azurerm" {
-    storage_account_name = var.existing_storage_account_name
-    container_name       = var.existing_container_name
+    resource_group_name  = "Aakash-Rg"
+    storage_account_name = "tfteststg1"
+    container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    resource_group_name  = var.existing_resource_group_name
   }
 }
+
 
