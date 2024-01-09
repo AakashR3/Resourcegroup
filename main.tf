7df8dad41279c9786620f790eb462068e56b4a91
AakashR3 <109940526+AakashR3@users.noreply.github.com>
2024-01-09 11:58:20 +0530

    Update main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+resource "azurerm_resource_group" "test" {
+  name     = "testing-tf"
+  location = "Central US"
+
+  tags = {
+    Region      = "Central US"
+    # Team        = var.team_tag
+    # Environment = "dev"
+    # Creator     = var.creator
+}
+}
