2d1315fba46d3e88c393c6ecabc1ac3e61c2c709
AakashR3 <109940526+AakashR3@users.noreply.github.com>
2024-01-09 14:55:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,3 +9,16 @@ resource "azurerm_resource_group" "test" {
     # Creator     = var.creator
 }
 }
+resource "azurerm_storage_account" "new" {
+  name                      = "terraformstorage${var.unique_id}"
+  resource_group_name       = azurerm_resource_group.new.name
+  location                  = azurerm_resource_group.new.location
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+}
+
+resource "azurerm_storage_container" "new_state_folder" {
+  name                   = "terraform-state-${upper(random_string.state_folder_suffix.result)}"
+  storage_account_name   = azurerm_storage_account.new.name
+  container_access_type  = "private"
+}
