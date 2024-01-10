3aad7db11d881d57fc217fc3538c17ddf68083de
AakashR3 <109940526+AakashR3@users.noreply.github.com>
2024-01-10 10:59:14 +0530

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,15 +1,3 @@
 locals {
   unique_id_uppercase = upper(var.unique_id)
 }
-
-locals {
-  backend_config = merge(
-    {
-      "storage_account_name" = "tfteststg1",
-      "container_name"       = "tfstate"
-      "key"                  = "terraform-${var.unique_id}/terraform.tfstate",
-      "resource_group_name"  = "Aakash-Rg",
-    },
-    var.backend_extra_config
-  )
-}
