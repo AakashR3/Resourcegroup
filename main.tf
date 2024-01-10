347a7770c443043411e4054d73020fdd763a6150
AakashR3 <109940526+AakashR3@users.noreply.github.com>
2024-01-10 11:54:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ resource "azurerm_resource_group" "test" {
 
   tags = {
     Region      = "Central US"
+    UniqueID    = var.unique_id
     # Team        = var.team_tag
     # Environment = "dev"
     # Creator     = var.creator
