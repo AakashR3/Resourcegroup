6bab7557f7ce066f9c3509cb20a396f41706a41e
AakashR3 <109940526+AakashR3@users.noreply.github.com>
2024-01-10 16:37:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "Aakash-Rg"
-    storage_account_name   = "tfteststg1"
-    container_name         = "tfstate"
-    key                    = "tfstate/${var.unique_id}/terraform.tfstate"
-  }
-}
-
 resource "azurerm_resource_group" "test" {
   name     = "terraform-rg-${var.unique_id}"
   location = "Central US"
