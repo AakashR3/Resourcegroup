262b8651c295988fe7a07792763a5d16fe45ffd1
AakashR3 <109940526+AakashR3@users.noreply.github.com>
2024-01-10 16:43:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "test" {
-  name     = "terraform-rg-${var.unique_id}"
+  name     = "Aakash-terraform-rg-${var.unique_id}"
   location = "Central US"
 
   tags = {
