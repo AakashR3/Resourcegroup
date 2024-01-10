92549bfb67cb4c358d48fb77dda242635a2ec5f9
AakashR3 <109940526+AakashR3@users.noreply.github.com>
2024-01-10 16:22:01 +0530

    Update backend-config.tf

--- backend-config.tf
+++ backend-config.tf
@@ -1,4 +1,4 @@
-resource_group_name   = "Aakash-Rg"
-storage_account_name   = "tfteststg1"
-container_name         = "tfstate"
-key                    = "tfstate/${unique_id}/terraform.tfstate"
+#resource_group_name   = "Aakash-Rg"
+#storage_account_name   = "tfteststg1"
+#container_name         = "tfstate"
+#key                    = "tfstate/${unique_id}/terraform.tfstate"
