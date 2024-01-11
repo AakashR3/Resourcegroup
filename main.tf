204607bdacddf65aa6ce69980566faed4c794f29
AakashR3 <109940526+AakashR3@users.noreply.github.com>
2024-01-11 15:30:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
 resource "azurerm_resource_group" "test" {
-  #name     = "Aakash-terraform-rg-${var.unique_id}"
-  name     = "Aakash-terraform-rg"
-  location = "Central US"
+  name     = var.resource_group_name
+  location = "Central India"
 
   tags = {
-    Region      = "Central US"
-    #UniqueID    = var.unique_id
-    # Team        = var.team_tag
+    Region      = "Central India"
+    # UniqueID    = var.unique_id  # Uncomment if you have a variable named unique_id
+    # Team        = var.team_tag    # Uncomment if you have a variable named team_tag
     # Environment = "dev"
     # Creator     = var.creator
   }
