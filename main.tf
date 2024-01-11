adbc5a047f420f5dca4493f3729641848e9760db
AakashR3 <109940526+AakashR3@users.noreply.github.com>
2024-01-11 15:34:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,20 @@
+data "azurerm_client_config" "current" {}
+
+data "azurerm_directory_object" "user" {
+  user_principal_name = var.user_email
+}
+
 resource "azurerm_resource_group" "test" {
   name     = var.resource_group_name
   location = "Central India"
 
   tags = {
-    Region      = "Central India"
-    # UniqueID    = var.unique_id  # Uncomment if you have a variable named unique_id
-    # Team        = var.team_tag    # Uncomment if you have a variable named team_tag
-    # Environment = "dev"
-    # Creator     = var.creator
+    Region = "Central India"
   }
 }
 
+resource "azurerm_role_assignment" "contributor_assignment" {
+  #scope                = azurerm_resource_group.test.id
+  role_definition_name = "Contributor"
+  principal_id         = data.azurerm_directory_object.user.id
+}
