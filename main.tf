4d9e49bed6ec6828c4cd0148404715e57e906518
AakashR3 <109940526+AakashR3@users.noreply.github.com>
2024-01-18 10:55:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,12 +6,4 @@ resource "azurerm_resource_group" "test" {
     Region = "Central India"
   }
 }
-data "azurerm_directory_object" "user" {
-  user_principal_name = var.client_id  # Replace with the actual user principal name
-}
 
-resource "azurerm_role_assignment" "contributor_assignment" {
-  scope                = azurerm_resource_group.test.id
-  role_definition_name = "Contributor"
-  principal_id         = data.azurerm_directory_object.user.id
-}
