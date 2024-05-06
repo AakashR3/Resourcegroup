edb013a63c84bbd552fc26c967f3480b35e5f498
AakashR3 <109940526+AakashR3@users.noreply.github.com>
2024-05-06 17:43:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,16 +6,10 @@ resource "azurerm_resource_group" "test" {
     Region = "Central India"
   }
 }
-#resource "azurerm_role_assignment" "owner" {
-#  scope                = azurerm_resource_group.test.id
-#  role_definition_name = "Owner"
-#  principal_id         = ""
-#}
-
 resource "azurerm_role_assignment" "owner" {
   scope                = azurerm_resource_group.test.id
   role_definition_name = "Owner"
-  principal_id         = null  # Placeholder, since we're not using it directly
-  principal_type       = "User"
-  #principal_name       = var.user_email
+  principal_id         = "93b48675-c174-49da-816f-82eda8dc3e1d"
 }
+
+
